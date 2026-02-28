95f110b84889162e5e62ce2a32bb6100238d2be6
Vishwajeet Patil <vishwjitpatil01@gmail.com>
2026-02-28 19:00:56 +0530

    complete homepage UI with templates and project form

--- src/app/(root)/page.jsx
+++ src/app/(root)/page.jsx
@@ -1,12 +1,32 @@
-import { Button } from "@/components/ui/button";
-import { UserButton } from "@clerk/nextjs";
-import Image from "next/image";
+ import React from 'react'
+ import Image from 'next/image'
+import ProjectsForm from '@/modules/home/components/project-form'
 
-export default function Home() {
+const Page = () => {
   return (
-    <div className="flex flex-clo items-center justify-center h-screen">
-      <Button>Test</Button>
-      <UserButton />
+    <div className="min-h-screen flex flex-col items-center justify-center px-4 py-16">
+      <div className="max-w-5xl w-full flex flex-col items-center text-center space-y-6">
+        <section className='space-y-8 flex flex-col items-center'>
+          <div className='flex flex-col items-center'>
+            <Image
+            src={"/logo.svg"}
+            width={100}
+            height={100}
+            alt='Logo'
+            className='hidden md:block invert dark:invert-0'
+          />
+          </div>
+        </section>
+        <h1 className="text-3xl md:text-6xl font-bold tracking-tight">Build Something with heart</h1>
+        <p className="text-lg md:text-xl text-muted-foreground max-w-2xl">
+            Create apps and websites by chatting with AI
+          </p>
+          <div className="w-full max-w-4xl mt-8">
+            <ProjectsForm/>
+          </div>
+      </div>
     </div>
-  );
+  )
 }
+
+export default Page
