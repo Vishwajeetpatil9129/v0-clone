f5773d379f77529a6187a0b97146b5a2be316dce
Vishwajeet Patil <vishwajeetpatil9129@gmail.com>
2026-06-26 00:16:52 +0530

    feat : Added a Landing Page

--- src/app/(root)/page.jsx
+++ src/app/(root)/page.jsx
@@ -1,37 +1,43 @@
 import React from 'react'
 import Image from 'next/image'
+import { SignedIn, SignedOut } from '@clerk/nextjs'
 import ProjectsForm from '@/modules/home/components/project-form'
 import ProjectList from '@/modules/home/components/project-list'
+import LandingPage from '@/modules/home/components/landing-page'
 
 const Page = () => {
   return (
-    <div className="min-h-screen flex items-start justify-center px-4"> 
-      
-      <div className="max-w-5xl w-full flex flex-col items-center text-center gap-5"> 
+    <>
 
-        <Image
-          src={"/logo.svg"}
-          width={80} 
-          height={80} 
-          alt='Logo'
-          className='invert dark:invert-0' 
-        />
+      <SignedOut>
+        <LandingPage />
+      </SignedOut>
 
-        <h1 className="text-3xl md:text-5xl font-bold tracking-tight"> 
-          Build Something with Knowledge.
-        </h1>
 
-        <p className="text-base md:text-lg text-muted-foreground max-w-xl"> 
-          Create apps and websites by chatting with AI.
-        </p>
-
-        <div className="w-full max-w-3xl"> 
-          <ProjectsForm/>
+      <SignedIn>
+        <div className="min-h-screen flex items-start justify-center px-4"> 
+          <div className="max-w-5xl w-full flex flex-col items-center text-center gap-5"> 
+            <Image
+              src={"/logo.svg"}
+              width={80} 
+              height={80} 
+              alt='Logo'
+              className='invert dark:invert-0' 
+            />
+            <h1 className="text-3xl md:text-5xl font-bold tracking-tight"> 
+              Build Something with Knowledge.
+            </h1>
+            <p className="text-base md:text-lg text-muted-foreground max-w-xl"> 
+              Create apps and websites by chatting with AI.
+            </p>
+            <div className="w-full max-w-3xl"> 
+              <ProjectsForm/>
+            </div>
+            <ProjectList/>
+          </div>
         </div>
-        <ProjectList/>
-
-      </div>
-    </div>
+      </SignedIn>
+    </>
   )
 }
 
